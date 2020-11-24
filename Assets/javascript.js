ae70ed75bd6b08418bb14372f6317c005ed59465
Derek Price <derekp098@gmail.com>
2020-11-23 16:44:55 -0800

    trying to use jquery to fix background jumping

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,1 +1,19 @@
 //Do something
+
+$(function(){
+
+    var $w = $(window),
+        $background = $('#background');
+  
+    // Fix background image jump on mobile
+    if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+      $background.css({'top': 'auto', 'bottom': 0});
+  
+      $w.resize(sizeBackground);
+      sizeBackground();
+    }
+  
+    function sizeBackground() {
+       $background.height(screen.height);
+    }
+  });
